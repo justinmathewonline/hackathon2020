232fdd64fb55064cf8f590f5c69997c3c13e83e2
Divya Janardhanan <Divya.Janardhanan@finastra.com>
2020-12-09 14:05:06 +0530

    Added header, menu bar, css file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddambulanceComponent } from '../app/addambulance/addambulance.component';
 import { RegistrationComponent } from '../app/registration/registration.component';
+import { HomePageComponent } from '../app/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: 'addambulance', component: AddambulanceComponent }
 ];
